650584806c88168c6f2efbebffc4a83991117881
Erez Shtayman <erezstmn@gmail.com>
2018-02-20 14:32:09 +0200

    Full functionality

--- server/server.js
+++ server/server.js
@@ -15,6 +15,7 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "PATCH");
     next();
   });
 
@@ -30,7 +31,62 @@ app.post('/movies', (req,res) => {
         res.status(400).send(e);
     })
 });
-
+app.get('/movies', (req,res) => {
+    let movie = req.query.movie_name;   
+    Movie.findOne({
+        "movie_name": movie
+    })
+    .then((movie) => {
+        res.send({movie});
+    }, (e) => {
+        res.status(400).send(e);
+    })
+});
+app.get('/movies/search', (req, res) => {
+    let genreToMatch = req.query.genre;
+    let fromYear = req.query.from -1;
+    let toYear = req.query.to +1;
+    
+    Movie.find({
+        genre: genreToMatch,
+        release_year:{
+            $gt: fromYear,
+            $lt: toYear
+        }
+    })
+    .then((movies) => {
+        res.send({movies});
+    }, (e) => {
+        res.status(400).send(e);
+    })
+});
+app.patch('/movies/', (req,res) => {
+    let movie = req.query.movie_name; 
+    let newScore = req.query.score;
+    Movie.findOneAndUpdate({
+        movie_name: movie
+    },{
+        $push: {
+            score: newScore
+        }
+    },{
+        returnOriginal: false
+    }). then((result) => {
+        res.send({result});
+    }, (e) =>{
+        res.status(400).send(e);
+    })
+});
+// app.get('/movies/:name', (req, res) => {
+//     let name =  req.params.name;
+//     let score = req.params.score;
+//     console.log(name, score);
+// })
+// .then((movie) => {
+//     res.send({movie});
+// }, (e) => {
+//     res.status(400).send(e);
+// })
 app.get('/',(req, res) => {
     res.send('test');
 });
